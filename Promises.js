a487b79a18a06762aa7fe4f9771fed02fd01d717
Hir2wa <alainfabricehirwa@gmail.com>
2025-04-24 11:34:09 +0200

    added the file

--- Promises.js
+++ Promises.js
@@ -2,5 +2,13 @@
 function realPromise() {
   new Promise(function (resolve, reject) {
     fetch();
+    //the fetch come with the parameters either string or object with a stringfier
+    //resorce parameter comes with a resource you wish to fetch
+    // the object comes with url
+
+    //an option comes with any option you can apply to your resources
+    //request unit an objrct that contain a settings that you can apply to the request
+
+    //a promise that resolve a request object
   });
 }
