325c19d267785df9819f0ad0605afe9c03d78e55
Hir2wa <alainfabricehirwa@gmail.com>
2025-04-28 14:50:10 +0200

    resolved the promise issues

--- tryAndCatch.js
+++ tryAndCatch.js
@@ -1,3 +1,29 @@
 function getData() {
-  return new Promisek(function (resolve, reject) {});
+  return new Promise(function (resolve, reject) {
+    resolve("Something went wrong");
+  });
 }
+
+async function start2(params) {
+  try {
+    let result = await getData();
+    console.log(` succefully: ${result}`);
+  } catch (error) {}
+
+  const result = await getData().catch((error) => {
+    console.log(`Error ${error}`);
+  });
+  console.log(error);
+}
+
+start2();
+
+// async function start(params) {
+//   try {
+//     const result = await getData();
+//     console.log(result);
+//   } catch (error) {
+//     console.log(`${error}`);
+//   }
+// }
+// start();
