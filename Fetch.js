ed9c5dda980f8782f006cfb24b264f91ccae5e7a
Hir2wa <alainfabricehirwa@gmail.com>
2025-05-05 11:56:17 +0200

    added some new changes

--- Fetch.js
+++ Fetch.js
@@ -9,3 +9,11 @@ async function getData(params) {
 getData();
 
 //fetch() is already a built-in Promise-based function.
+//ohh forgetting to put await there  that means we will be working with a pending promise instead of actualy data
+
+// 🧠 Bonus Tip: res.text() vs res.json()
+// Use res.text() if the response is plain text.
+
+// Use res.json() if the response is JSON (which is usually the case for APIs).
+
+// There’s also res.blob() and res.formData() for files and forms.
