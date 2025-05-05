6a6153b4970c4ce42ad52612589eed57c2dd96f0
Hir2wa <alainfabricehirwa@gmail.com>
2025-05-05 14:53:57 +0200

    added map in the fetchs and apply some good thing sir

--- AsyncIteractions.js
+++ AsyncIteractions.js
@@ -29,3 +29,30 @@ async function fetchAllPosts() {
 }
 
 fetchAllPosts();
+
+const arls = [
+  "https://jsonplaceholder.typicode.com/posts/1",
+  "https://jsonplaceholder.typicode.com/posts/2",
+  "https://jsonplaceholder.typicode.com/posts/3",
+];
+
+async function fetchAllParallel() {
+  try {
+    const fetchPromises = arls.map(async (url) => {
+      const res = await fetch(url);
+      const data = await res.json();
+      return data.title;
+    });
+
+    const titles = await Promise.all(fetchPromises);
+
+    // Log all titles after all fetches complete
+    titles.forEach((title, index) => {
+      console.log(`Post ${index + 1} title:`, title);
+    });
+  } catch (err) {
+    console.error("Something went wrong 💥", err);
+  }
+}
+
+fetchAllParallel();
