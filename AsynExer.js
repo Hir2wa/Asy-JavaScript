c6548b2c896fec589630575082db6a83ecca7cc4
Hir2wa <alainfabricehirwa@gmail.com>
2025-05-13 10:20:53 +0200

    added retries in my fetch api

--- AsynExer.js
+++ AsynExer.js
@@ -1,11 +1,26 @@
 async function fetchFirstUserPostsWithRetries() {
   let url = "https://jsonplaceholder.typicode.com/users";
-  let response;
-  if (response == "rejected") {
-    for (let index = 0; index <= 2; index++) {
-      response = await fetch(url);
+  let attempts = 0;
+  let retries = 3;
+
+  while (attempts < retries) {
+    try {
+      let response = await fetch(url);
+      if (!response.ok) {
+        console.log("HTTP Error! Status :  " + response.status);
+      }
       let data = await response.json();
-      console.log(data);
+      console.log("Data Fetched:  ", data);
+      return data;
+    } catch (error) {
+      console.log(`Failed  Attempt  ${attempts + 1} : `, error.message);
+
+      attempts++;
+
+      if (attempts >= retries) {
+        console.log("All retries failed");
+        return null;
+      }
     }
   }
 }
