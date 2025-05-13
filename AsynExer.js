a857f4eb97497b833254cf7c293caaa98076d1f6
Hir2wa <alainfabricehirwa@gmail.com>
2025-05-13 11:11:26 +0200

    added fetch method with the functionalities

--- AsynExer.js
+++ AsynExer.js
@@ -10,7 +10,18 @@ async function fetchFirstUserPostsWithRetries() {
         console.log("HTTP Error! Status :  " + response.status);
       }
       let data = await response.json();
-      console.log("Data Fetched:  ", data);
+      let users = data.map((users) => users.id);
+      console.log(typeof users);
+
+      console.log(`All Users: ${users}`);
+
+      console.log("Data Fetched:  ", data[0].id);
+      let newObject = {};
+      for (const user of data) {
+        newObject[user.id] = user.name;
+      }
+      console.log(`Object(name,id)`, newObject);
+
       return data;
     } catch (error) {
       console.log(`Failed  Attempt  ${attempts + 1} : `, error.message);
