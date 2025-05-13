83b50b6a7b439036ee0ffe448147eb488fffb7cd
Hir2wa <alainfabricehirwa@gmail.com>
2025-05-13 09:54:19 +0200

    added loop to run

--- AsynExer.js
+++ AsynExer.js
@@ -1,5 +1,6 @@
 async function fetchFirstUserPostsWithRetries() {
   let url = "https://jsonplaceholder.typicode.com/users";
+  for (let index = 0; sd; index++) {}
   let res = await fetch(url);
   let data = await res.json();
   console.log(data);
