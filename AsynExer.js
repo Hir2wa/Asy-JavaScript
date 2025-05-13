18bf52c8ddc63af46d71874cf867d15762f3713f
Hir2wa <alainfabricehirwa@gmail.com>
2025-05-13 09:59:05 +0200

    functionality on it

--- AsynExer.js
+++ AsynExer.js
@@ -1,9 +1,13 @@
 async function fetchFirstUserPostsWithRetries() {
   let url = "https://jsonplaceholder.typicode.com/users";
-  for (let index = 0; sd; index++) {}
-  let res = await fetch(url);
-  let data = await res.json();
-  console.log(data);
+  let response;
+  if (response == "rejected") {
+    for (let index = 0; index <= 2; index++) {
+      response = await fetch(url);
+      let data = await response.json();
+      console.log(data);
+    }
+  }
 }
 
 fetchFirstUserPostsWithRetries();
