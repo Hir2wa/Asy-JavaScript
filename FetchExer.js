fdbb98b41e0791752925591988d4050a357d0817
Hir2wa <alainfabricehirwa@gmail.com>
2025-05-22 15:49:28 +0200

    added new promise.all

--- FetchExer.js
+++ FetchExer.js
@@ -6,15 +6,45 @@
 // ✅ What you’ll use:
 // fetch
 // .map()
-async function usersInfo() {
-  let url = " https://jsonplaceholder.typicode.com/users";
-  let res = await fetch(url);
-  let data = await res.json();
-  console.log(data);
+// async function usersInfo() {
+//   let url = " https://jsonplaceholder.typicode.com/users";
+//   let res = await fetch(url);
+//   let data = await res.json();
+//   console.log(data);
 
-  let users = data.map((usernm) => usernm.name);
+//   let users = data.map((usernm) => usernm.name);
 
-  console.log(users);
+//   console.log(users);
+// }
+
+// usersInfo();
+
+// async function postInfo() {
+//   let res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   let data = await res.json();
+//   console.log(data);
+// }
+
+// postInfo();
+
+async function loadData() {
+  const usersURL = "https://jsonplaceholder.typicode.com/users";
+  const postsURL = "https://jsonplaceholder.typicode.com/posts";
+
+  try {
+    const [usersRes, postsRes] = await Promise.all([
+      fetch(usersURL),
+      fetch(postsURL),
+    ]);
+
+    const users = await usersRes.json();
+    const posts = await postsRes.json();
+
+    console.log("Users ", users);
+    console.log("Posts ", posts);
+  } catch (error) {
+    console.error("❌Error loading data:", error);
+  }
 }
 
-usersInfo();
+loadData();
